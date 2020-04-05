ec6d4bb1e0c34a7ef2af193e039d9902a87838a2
Konstantin Arapov <konstantin@roxosoft.com>
2020-04-05 16:29:23 +0500

    Fix merging issues.

--- borsvarlden/Models/FinwireNew.cs
+++ borsvarlden/Models/FinwireNew.cs
@@ -12,6 +12,7 @@ namespace borsvarlden.Models
         public string PathRelative { get; set; }
         public DateTime Date { get; set; }
         public string Title { get; set; }
+        public string NewsText { get; set; }
 
         //todo with flags
         public bool FinautoPassed { get; set;}
