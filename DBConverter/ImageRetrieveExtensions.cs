57d8df76fa84810d3cb08cafda0f3eb604fe1c17
Konstantin Arapov <konstantin@roxosoft.com>
2020-05-25 23:18:54 +0500

    Converter MySQL => MS SQL. Converted main data.

---
+++ DBConverter/ImageRetrieveExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DBConverter
+{
+    public static class ImageRetrieveExtensions
+    {
+        private static string _imagesPath = Path.GetFullPath($@"{Directory.GetCurrentDirectory()}\..\..\..\..\borsvarlden\wwwroot\assets\images\finauto");
+        private static List<string> _imageFilesAvailable = new List<string>();
+
+        static ImageRetrieveExtensions()
+        {
+            new List<string>
+            {
+                $@"{_imagesPath}\company",
+                $@"{_imagesPath}\socialtag",
+                $@"{_imagesPath}\other"
+
+            }.ForEach(x =>
+                {
+                    Directory.GetDirectories(x)
+                        .ToList()
+                        .ForEach(y => _imageFilesAvailable.AddRange(Directory.GetFiles(y).ToList()));
+                }
+            );
+        }
+
+        public static string ToNewImageFilePath(this string inputPath)
+        {
+            var r = _imageFilesAvailable.FirstOrDefault(x=> x.Contains(inputPath.Substring(inputPath.LastIndexOf('/') + 1)));
+
+            if (r == null)
+                Console.WriteLine(inputPath);
+
+            r = r?.Substring(r.IndexOf("wwwroot") + 8).Replace('\\', '/');
+
+            return r;
+        }
+    }
+}
---
+++ borsvarlden.Tests.UnitTests/Tests/TestTools/TestDBConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DBConverter;
+using NUnit.Framework;
+
+namespace borsvarlden.Tests.UnitTests.Tests.TestTools
+{
+    [TestFixture]
+    class TestDBConverter
+    {
+        [TestCase("2018/04/bvfa-c-apple-0012.jpg")]
+        public void TestToNewImageFilePath(string path)
+        {
+            var r = path.ToNewImageFilePath();
+            path.ToNewImageFilePath();
+        }
+
+        [TestCase("2020/05/image0052-960x540.jpg")]
+        public void TestImageSaver(string urlFragment)
+        {
+            ImageSaver.Save(urlFragment, @".\");
+        }
+    }
+}
