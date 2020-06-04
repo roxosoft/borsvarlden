549373dfe9e924f2c90e5bd67210d60f52240c65
Konstantin Arapov <konstantin@roxosoft.com>
2020-06-04 09:07:03 +0500

    Moved seeding news from files to endpoint.

--- borsvarlden/Controllers/Api/FinwireController.cs
+++ borsvarlden/Controllers/Api/FinwireController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,29 @@ namespace borsvarlden.Controllers.Api
             _logger.LogWarning($"API updeatenews uid={uid}");
             var data = await _finwireParserService.ParseXmlContent(xml);
             _finwireNewsService.AddSingleNews(data);
+            return "OK";
+        }
+
+        [HttpGet]
+        [Route("api/seednews")]
+        public async Task<string> SeedNews()
+        {
+            var pathBase = Path.GetFullPath($@"{Directory.GetCurrentDirectory()}\..\TestData\FinwireFiles");
+
+            for (int i = 1; i <= 8; i++)
+            {
+                //there is no data
+                if (i == 4 || i == 5)
+                    continue;
+
+                var path = $@"{pathBase}\{i.ToString("D2")}";
+
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    var finwireData = _finwireParserService.ParseFile(file);
+                    _finwireNewsService.AddSingleNews(finwireData.Result);
+                }
+            }
             return "OK";
         }
     }
