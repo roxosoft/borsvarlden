289f781989c84dc30d2f3700ce7cc0c8ed3bf4e1
Konstantin Arapov <konstantin@roxosoft.com>
2020-06-10 13:51:13 +0500

    BSVLD-64 Use Banners endpoint to display banners

---
+++ borsvarlden/Helpers/BannerHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace borsvarlden.Helpers
+{
+    public static class BannerHelper
+    {
+        private static readonly string _url = "https://borsvarlden-banners.azurewebsites.net/getslider.php";
+
+        public static async Task<string> GetHtmlAsync()
+        {
+            using (var response = new StreamReader(((HttpWebResponse)await WebRequest.Create(_url).GetResponseAsync())?.GetResponseStream()))
+            {
+                var res = response.ReadToEnd();
+                return res;
+            }
+        }
+    }
+}
