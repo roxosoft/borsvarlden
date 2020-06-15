c44d2d9d214ed23ef2c1e01e25969ad43e7481e3
Konstantin Arapov <konstantin@roxosoft.com>
2020-06-15 13:45:01 +0500

    Bugfixes cookies desclaimer.

--- borsvarlden/wwwroot/js/cookie-notice.js
+++ borsvarlden/wwwroot/js/cookie-notice.js
@@ -28,12 +28,12 @@
 		
 		self = this;
 
-		// set expiry time in seconds
-		cnLater.setTime( parseInt( cnTime.getTime() ) + parseInt( 'month' ) * 1000 );
+		// set expiry time in seconds - two years
+        cnLater.setTime(parseInt(cnTime.getTime()) + 64281600 * 1000 );
 
 		// set cookie
 		cookie_value = cookie_value === 'accept' ? true : false;
-        document.cookie = 'cookie_notice_accepted' + '=' + cookie_value + ';expires=' + cnLater.toGMTString() + ';' + ( /*cnArgs.cookieDomain !== undefined && cnArgs.cookieDomain !== '' ? 'domain=' + cnArgs.cookieDomain + ';' :*/ '' ) + ( /*cnArgs.cookiePath !== undefined && cnArgs.cookiePath !== '' ? 'path=' + cnArgs.cookiePath + ';' :*/ '' );
+        document.cookie = 'cookie_notice_accepted' + '=' + cookie_value + ';expires=' + cnLater.toGMTString() + ';'; //+ ( /*cnArgs.cookieDomain !== undefined && cnArgs.cookieDomain !== '' ? 'domain=' + cnArgs.cookieDomain + ';' :*/ '' ) + ( /*cnArgs.cookiePath !== undefined && cnArgs.cookiePath !== '' ? 'path=' + cnArgs.cookiePath + ';' :*/ '' );
 
 		// trigger custom event
 		$.event.trigger( {
