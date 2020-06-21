2562d0bd6644f52b9eaddb7e88967265217d41b0
Konstantin Arapov <konstantin@roxosoft.com>
2020-06-21 18:15:35 +0500

    Bugfix owl-carousel. Added relalated news block fix invisible when not finwire.

---
+++ borsvarlden/wwwroot/js/detailarticle.js
@@ -0,0 +1,19 @@
+﻿(function ($) {
+$(document).ready(function () {
+    var carousel = $(".owl-carousel").owlCarousel({
+        margin: 30,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            1020: {
+                items: 4
+            }
+        }
+    });
+});
+})(jQuery);
