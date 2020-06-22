055fbd8d9ccc19ae6c7341de04323c90381d1bc7
Konstantin Arapov <konstantin@roxosoft.com>
2020-06-22 13:41:00 +0500

    BSVLD-78

---
+++ borsvarlden/wwwroot/js/sassy-social-share.js
@@ -0,0 +1,13 @@
+﻿function heateorSssLoadEvent(e) {
+    var t = window.onload;
+    if (typeof window.onload != "function") {
+        window.onload = e;
+    } else {
+         window.onload = function () { t(); e() }
+    }
+};
+
+function heateorSssPopup(e) {
+     window.open(e, "popUpWindow", "height=400,width=600,left=400,top=100,resizable,scrollbars,toolbar=0,personalbar=0,menubar=no,location=no,directories=no,status")
+};
+var heateorSssWhatsappShareAPI = "web";
