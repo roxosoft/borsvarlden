904f73fd4db184aaf137434f69f72688143ac9ca
Tim Fedoseev <tim.fedoseev@gmail.com>
2020-06-24 17:01:19 +0300

    BSVLD-81 [UI] Character reference for "An ampersand" displays in news description

---
+++ borsvarlden/Migrations/20200624134414_NewsFixAmps.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace borsvarlden.Migrations
+{
+    public partial class NewsFixAmps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update [FinwireNews] set title = replace(title, '&amp;', '&') where title like '%&amp;%'");
+            migrationBuilder.Sql("update [FinwireNews] set subtitle = replace(subtitle, '&amp;', '&') where subtitle like '%&amp;%'");
+            migrationBuilder.Sql("update [FinwireNews] set newstext = replace(newstext, '&amp;', '&') where newstext like '%&amp;%'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update [FinwireNews] set title = replace(title, '&', '&amp;') where title like '%&%'");
+            migrationBuilder.Sql("update [FinwireNews] set subtitle = replace(subtitle, '&', '&amp;') where subtitle like '%&%'");
+            migrationBuilder.Sql("update [FinwireNews] set newstext = replace(newstext, '&', '&amp;') where newstext like '%&%'");
+        }
+    }
+}
