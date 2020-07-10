1dbeec949ac91ecdb41e15d487de3ff2a1685a3e
Konstantin Arapov <konstantin@roxosoft.com>
2020-07-10 19:28:24 +0500

    Moved banner right to shared component. Updated right banner content.

---
+++ borsvarlden/Views/Shared/Components/BannerRight/BannerRight.cs
@@ -0,0 +1,23 @@
+﻿using borsvarlden.Helpers;
+using borsvarlden.Services.Entities;
+using borsvarlden.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace borsvarlden.Views.Shared.Components.BannerTop
+{
+    public class BannerRight : ViewComponent
+    {
+        public BannerRight()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("BannerRight");
+        }
+    }
+}
