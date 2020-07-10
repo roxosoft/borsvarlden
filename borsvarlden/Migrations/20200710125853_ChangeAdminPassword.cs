90dc0fcea111adf51edfb8c83266c7c5f33ae20b
Konstantin Arapov <konstantin@roxosoft.com>
2020-07-10 18:37:48 +0500

    Changed admin password.

---
+++ borsvarlden/Migrations/20200710125853_ChangeAdminPassword.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace borsvarlden.Migrations
+{
+    public partial class ChangeAdminPassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData("ApplicationUsers", "UserName", "admin", "PasswordHash",  "26C47F623988A3B69053047DD79CA5A7");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData("ApplicationUsers", "UserName", "admin", "PasswordHash", "21232F297A57A5A743894A0E4A801FC3");
+        }
+    }
+}
