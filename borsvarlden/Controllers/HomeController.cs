4200eaf4d9e0e856004a542713722c26094f83e8
Konstantin Arapov <konstantin@roxosoft.com>
2020-08-22 17:24:03 +0500

    BSVLD-97

--- borsvarlden/Controllers/HomeController.cs
+++ borsvarlden/Controllers/HomeController.cs
@@ -1,5 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceModel.Syndication;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using borsvarlden.Models;
@@ -128,6 +134,46 @@ namespace borsvarlden.Controllers
         {
             return View();
         }
+
+        [ResponseCache(Duration = 1200)]
+        [HttpGet]
+        public async Task<IActionResult> Rss()
+        {
+            var url = $"{Request.Scheme}://{Request.Host}";
+            var feed = new SyndicationFeed("Börsvärlden - Maximera din digitala synlighet",
+                "Modern distributionskonsult inom börs, finans och trading med fokus på digital kommunikation, paketering och spridning av finansiella nyheter och koncept", 
+                new Uri(url),
+                "Börsvärlden", 
+                DateTime.Now);
+            
+            feed.Copyright = new TextSyndicationContent($"{DateTime.Now.Year} Börsvärlden");
+            var items = new List<SyndicationItem>();
+
+
+            (await _finwireNewsService.GetNewsForFeedingWithPrio(20)).ForEach(x =>
+                items.Add(
+                    new SyndicationItem(x.Title, x.Subtitle, new Uri($@"{url}/artiklar/{x.Slug}"), x.Id.ToString(), x.Date)));
+
+            feed.Items = items;
+            var settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                NewLineHandling = NewLineHandling.Entitize,
+                NewLineOnAttributes = true,
+                Indent = true
+            };
+            using (var stream = new MemoryStream())
+            {
+                using (var xmlWriter = XmlWriter.Create(stream, settings))
+                {
+                    var rssFormatter = new Rss20FeedFormatter(feed, false);
+                    rssFormatter.WriteTo(xmlWriter);
+                    xmlWriter.Flush();
+                }
+                return File(stream.ToArray(), "application/rss+xml; charset=utf-8");
+            }
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
