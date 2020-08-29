09c1b5dbf718d08292112bc93f9586421881ca3c
Konstantin Arapov <konstantin@roxosoft.com>
2020-08-29 22:43:53 +0500

    BSVLD-97-02 complete

--- borsvarlden/Controllers/RSSController.cs
+++ borsvarlden/Controllers/RSSController.cs
@@ -26,52 +26,76 @@ namespace borsvarlden.Controllers
 
         [ResponseCache(Duration = 1200)]
         [HttpGet]
+        [Route("feed/borskollen")]
         public async Task<IActionResult> Index()
         {
-            var url = $"{Request.Scheme}://{Request.Host}";
-            var feed = new SyndicationFeed(title: "Börsvärlden - Maximera din digitala synlighet",
+            var urlSiteRoot = $"{Request.Scheme}://{Request.Host}";
+            var feed = new SyndicationFeed(title: "Börsvärlden",
                 description: "Modern distributionskonsult inom börs, finans och trading med fokus på digital kommunikation, paketering och spridning av finansiella nyheter och koncept",
-                feedAlternateLink: new Uri(url),
+                feedAlternateLink: new Uri(urlSiteRoot),
                 id: "Börsvärlden",
                 lastUpdatedTime: DateTime.Now);
-
-            var baseNamespace = "http://www.w3.org/2000/xmlns/";
-
-            XmlQualifiedName n = new XmlQualifiedName("content", baseNamespace);
-          
 
             void AddNamespace(string nameSpaceName, string nameSpaceUrl)
                 => feed.AttributeExtensions.Add(new XmlQualifiedName(nameSpaceName, "http://www.w3.org/2000/xmlns/"), nameSpaceUrl);
 
             var contentNs = "http://purl.org/rss/1.0/modules/content/";
+            var wfwNs = "http://wellformedweb.org/CommentAPI/";
+            var dcNs = "http://purl.org/dc/elements/1.1/";
+            var atomNs = "http://www.w3.org/2005/Atom";
+            var syNs = "http://purl.org/rss/1.0/modules/syndication/";
+            var slashNs = "http://purl.org/rss/1.0/modules/slash/";
+
             AddNamespace("content", contentNs);
+            AddNamespace("wfw", wfwNs);
+            AddNamespace("dc", dcNs);
+            AddNamespace("atom", atomNs);
+            AddNamespace("sy", syNs);
+            AddNamespace("slash", slashNs);
 
-            //feed.AttributeExtensions.Add(n, "http://purl.org/rss/1.0/modules/content/");
+            feed.ElementExtensions.Add("updatePeriod", syNs, "hourly");
+            feed.ElementExtensions.Add("updateFrequency", syNs, "12");
+            feed.ElementExtensions.Add("language", String.Empty, "en-US");
+            XNamespace atom = "http://www.w3.org/2005/Atom";
+            feed.ElementExtensions.Add(new XElement(atom + "link",
+                new XAttribute("href", $"{urlSiteRoot}/feed/borskollen"),
+                new XAttribute("rel", "self"),
+                new XAttribute("type", "application/rss+xml")));
+            var xmlImage = new XElement("image");
 
-            feed.Copyright = new TextSyndicationContent($"{DateTime.Now.Year} Börsvärlden");
+            void AddChildToImage(string name, string value)
+            {
+                var childUrl = new XElement(name);
+                childUrl.Value = value;
+                xmlImage.Add(childUrl);
+            }
 
+            AddChildToImage("url", $"{urlSiteRoot}/assets/uploads/2016/10/borsvarlden_logo_rss.png");
+            AddChildToImage("title", "Börsvärlden");
+            AddChildToImage("link", urlSiteRoot);
+            AddChildToImage("width", "144");
+            AddChildToImage("height","144");
 
-
+            feed.ElementExtensions.Add(xmlImage);
             var items = new List<SyndicationItem>();
 
-
             (await _finwireNewsService.GetNewsForFeedingWithPrio(20)).ForEach(x =>
-            {
-
-                var item = new SyndicationItem(x.Title, x.Subtitle, new Uri($@"{url}/artiklar/{x.Slug}"),
-                    x.Id.ToString(), x.Date);
-                //item.ElementExtensions.Add("fooTag", String.Empty, "foo2");
-                var r = new XElement("guid", new XAttribute("isPermaLink", "false"));
-                r.Value = $@"{url}/artiklar/{x.Id.ToString()}";
-
-                item.ElementExtensions.Add(r.CreateReader());
-                item.ElementExtensions.Add("encoded", contentNs, "foo");
-
-
-                //var el = new XmlElement("","","");
-
-                items.Add(item);
-            }
+                {
+                    var item = new SyndicationItem(title:x.Title,x.Subtitle, new Uri($@"{urlSiteRoot}/artiklar/{x.Slug}"),
+                        x.Id.ToString(), x.Date);
+                    var guid = new XElement("guid", new XAttribute("isPermaLink", "false"));
+                    guid.Value = $@"{urlSiteRoot}/artiklar/{x.Id.ToString()}";
+                    item.ElementExtensions.Add(guid.CreateReader());
+                    item.ElementExtensions.Add(new XElement("enclosure", 
+                                           new XAttribute("url", $"{urlSiteRoot}/{x.ImageRelativeUrl}"), 
+                                           new XAttribute("type","image/jpg")));
+                    item.ElementExtensions.Add("pubDate", String.Empty, x.Date.ToString());
+                    item.ElementExtensions.Add("creator", dcNs, "<![CDATA[Börsvärlden]]>");
+                    item.ElementExtensions.Add("encoded", contentNs, $"<![CDATA[{x.NewsText}]]>");
+                    item.ElementExtensions.Add("commentRss", wfwNs, $"{urlSiteRoot}/artiklar/{x.Slug}");
+                    item.ElementExtensions.Add("comments", slashNs, "0");
+                    items.Add(item);
+                }
             );
 
             feed.Items = items;
@@ -86,15 +110,8 @@ namespace borsvarlden.Controllers
             {
                 using (var xmlWriter = XmlWriter.Create(stream, settings))
                 {
-
                     var rssFormatter = new Rss20FeedFormatter(feed, false);
-
-
-                    //xmlWriter.WriteAttributeString("xmlns", "gx", null, "http://www.google.com/kml/ext/2.2");
                     rssFormatter.WriteTo(xmlWriter);
-                    //const string xsi = "http://www.w3.org/2001/XMLSchema-instance";
-                    //xmlWriter.WriteAttributeString("xmlns", "xsi", "http://www.w3.org/2000/xmlns/", xsi);
-
                     xmlWriter.Flush();
                 }
                 return File(stream.ToArray(), "application/rss+xml; charset=utf-8");
