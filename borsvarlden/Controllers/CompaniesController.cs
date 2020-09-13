13369474591e0b6f3bca542fc7394d8d830a019f
Konstantin Arapov <konstantin@roxosoft.com>
2020-09-14 01:25:20 +0500

    BSVLD-106

---
+++ borsvarlden/Controllers/CompaniesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using borsvarlden.Services.Entities;
+using Microsoft.AspNetCore.Mvc;
+using borsvarlden.Models;
+
+namespace borsvarlden.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private IFinwireCompaniesService _finwireCompaniesService;
+        private IFinwireNewsService _finwireNewsService;
+
+        public CompaniesController(IFinwireCompaniesService finwireCompaniesService, IFinwireNewsService finwireNewsService)
+        {
+            _finwireCompaniesService = finwireCompaniesService;
+            _finwireNewsService = finwireNewsService;
+        }
+
+        [Route("CompaniesList/{letter}")]
+        [Route("Foretag/{letter}")]
+        public async Task<IActionResult> CompaniesList(string letter)
+        {
+            var listCompanies = await _finwireCompaniesService.GetListFinwireCompaniesByLetter(letter);
+            return View(listCompanies);
+        }
+
+        [Route("Foretagsinfo/{id}")]
+        public async Task<IActionResult> CompanyInfo(int id)
+        {
+            var company = await _finwireCompaniesService.GetFinwireCompany(id);
+            var res = await _finwireNewsService.GetFinwireNewWithCompany(id);
+            return View(new Tuple<FinwireCompany, List<FinwireNew>>(company, res));
+        }
+    }
+}
---
+++ borsvarlden/wwwroot/js/datagrid-companies.js
@@ -0,0 +1,80 @@
+﻿var selectedNewCompany;
+
+$(function () {
+
+    $("#articles-grid").dxDataGrid({
+        dataSource: DevExpress.data.AspNet.createStore({
+            key: "id",
+            
+            loadUrl: "../api/Companies/List",
+           // insertUrl: "api/Articles/Insert",
+            updateUrl: "../api/Companies/UpdateCompanyInfo"//,
+           // deleteUrl: "api/Articles/Delete"
+        }),
+        columns: [
+            {
+                dataField: "company",
+                caption: "company",
+                sortIndex: 0,
+                allowSorting: true,
+                sortOrder: "asc"
+            },
+            {
+                dataField: "description",
+                visible: false,
+                editCellTemplate: (itemElement, cellInfo) => {
+
+                    ($("<textarea>", { "id": "ckpeditor", "val": cellInfo.value })).appendTo(itemElement);
+
+                    $("<script>").append(CKEDITOR.replace("ckpeditor", {
+                        extraPlugins: 'embed',
+                        embed_provider: '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}&api_key=90d4029ed5529f8b49bbdc',
+                        filebrowserBrowseUrl: '/lib/fileman/index.html',
+                        filebrowserImageBrowseUrl: '/lib/fileman/index.html' + '?type=image',
+                        removeDialogTabs: 'link:upload;image:upload'
+                    })).appendTo(itemElement);
+
+                    $("<script>").append(CKEDITOR.instances.ckpeditor.on("change", function () { cellInfo.setValue(CKEDITOR.instances.ckpeditor.getData()) })).appendTo(itemElement);
+
+                }
+            }
+            /*{
+                dataField: "description",
+                caption: "description"
+            }*/
+        ],
+        editing: {
+            mode: "popup",
+            allowUpdating: true,
+            allowAdding: true,
+            allowDeleting: true,
+            popup: {
+                title: "Companies",
+                showTitle: true,
+                my: "top",
+                at: "top",
+                of: window
+            },
+            form: {
+                colCount: 2,
+                items: [//"company",
+                    {
+                        colSpan: 2,
+                        dataField: "description"
+                    }
+                ]
+            }
+        },
+        remoteOperations: true,
+        searchPanel: {
+            visible: true
+        },
+        grouping: {
+            autoExpandAll: false
+        },
+        filterRow: {
+            visible: true,
+            applyFilter: "onClick"
+        }
+    });
+});
