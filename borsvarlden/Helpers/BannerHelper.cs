0b8289ccda28f73c18b729c57153a7037649d4db
Konstantin Arapov <konstantin@roxosoft.com>
2020-12-05 18:55:36 +0500

    Fix banners error

--- borsvarlden/Helpers/BannerHelper.cs
+++ borsvarlden/Helpers/BannerHelper.cs
@@ -5,33 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
 
+
 namespace borsvarlden.Helpers
 {
+    using  Extensions;
+
     public static class BannerHelper
     {
         private static readonly string _url = "https://borsvarlden-banners.azurewebsites.net/getslider.php";
         private static readonly string _url_param = "https://borsvarlden-banners.azurewebsites.net/getslider-details.php?slider={0}";
 
+
         public static async Task<string> GetHtmlAsync()
         {
-            var res = string.Empty;
-
-            try
+            using (var response = new StreamReader(((HttpWebResponse)await WebRequest.Create(_url).GetResponseAsync())?.GetResponseStream()))
             {
-                using (var response =
-                    new StreamReader(((HttpWebResponse) await WebRequest.Create(_url).GetResponseAsync())
-                        ?.GetResponseStream()))
-                {
-                    res = response.ReadToEnd();
-                    return res;
-                }
+                var res = response.ReadToEnd();
+                return res;
             }
-            catch (Exception e)
-            {
-                //When error just output empty string (show nothing)
-            }
-
-            return res;
         }
 
         public static async Task<string> GetHtmlAsync(string slider)
@@ -39,6 +30,10 @@ namespace borsvarlden.Helpers
             using (var response = new StreamReader(((HttpWebResponse)await WebRequest.Create(String.Format(_url_param, slider)).GetResponseAsync())?.GetResponseStream()))
             {
                 var res = response.ReadToEnd();
+
+                if (res.Contains("Revolution Slider Error: Slider with alias"))
+                    return string.Empty;
+
                 return res;
             }
         }
