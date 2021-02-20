9641a75421d25858bbb1c0780065f84878410cb0
Konstantin Arapov <konstantin@roxosoft.com>
2021-02-20 13:20:18 +0500

    Restored previous version of splash ad script.

--- borsvarlden/wwwroot/js/cookie-notice.js
+++ borsvarlden/wwwroot/js/cookie-notice.js
@@ -1,28 +1,28 @@
-( function ( $ ) {
+(function ($) {
 
     $(document).ready(function () {
- 
-		var cnDomNode = $( '#cookie-notice' );
 
-		// handle set-cookie button click
-		$( document ).on( 'click', '.cn-set-cookie', function ( e ) {
-			e.preventDefault();
-			$( this ).setCookieNotice( $( this ).data( 'cookie-set' ) );
-		} );
+        var cnDomNode = $('#cookie-notice');
 
-		// display cookie notice
-		if ( document.cookie.indexOf( 'cookie_notice_accepted' ) === -1 ) {
-            cnDomNode.fadeIn( 300 );
-            $( 'body' ).addClass( 'cookies-not-accepted' );
-		} else {
-			cnDomNode.removeCookieNotice();
+        // handle set-cookie button click
+        $(document).on('click', '.cn-set-cookie', function (e) {
+            e.preventDefault();
+            $(this).setCookieNotice($(this).data('cookie-set'));
+        });
+
+        // display cookie notice
+        if (document.cookie.indexOf('cookie_notice_accepted') === -1) {
+            cnDomNode.fadeIn(300);
+            $('body').addClass('cookies-not-accepted');
+        } else {
+            cnDomNode.removeCookieNotice();
         }
 
-        setTimeout(
+        setInterval(
             function () {
                 $(this).setSplashScreenNotice($(this).data('splashScreen-set'));
             },
-            10000  
+            10000
         );
 
     });
@@ -44,51 +44,44 @@
     }
 
     // set Cookie Notice
-	$.fn.setCookieNotice = function ( cookie_value ) {
+    $.fn.setCookieNotice = function (cookie_value) {
 
-		var cnTime		= new Date(),
-			cnLater		= new Date(),
-			cnDomNode	= $( '#cookie-notice' );
-		
-		self = this;
+        var cnTime = new Date(),
+            cnLater = new Date(),
+            cnDomNode = $('#cookie-notice');
 
-		// set expiry time in seconds - two years
-        cnLater.setTime(parseInt(cnTime.getTime()) + 64281600 * 1000 );
+        self = this;
 
-		// set cookie
-		cookie_value = cookie_value === 'accept' ? true : false;
+        // set expiry time in seconds - two years
+        cnLater.setTime(parseInt(cnTime.getTime()) + 64281600 * 1000);
+
+        // set cookie
+        cookie_value = cookie_value === 'accept' ? true : false;
         document.cookie = 'cookie_notice_accepted' + '=' + cookie_value + ';expires=' + cnLater.toGMTString() + ';'; //+ ( /*cnArgs.cookieDomain !== undefined && cnArgs.cookieDomain !== '' ? 'domain=' + cnArgs.cookieDomain + ';' :*/ '' ) + ( /*cnArgs.cookiePath !== undefined && cnArgs.cookiePath !== '' ? 'path=' + cnArgs.cookiePath + ';' :*/ '' );
-      
-		// trigger custom event
-		$.event.trigger( {
-			type		: "setCookieNotice",
-			value		: cookie_value,
-			time		: cnTime,
-			expires		: cnLater
-		} );
 
-		cnDomNode.fadeOut( 300, function () {
-				self.removeCookieNotice();
-			} );
+        // trigger custom event
+        $.event.trigger({
+            type: "setCookieNotice",
+            value: cookie_value,
+            time: cnTime,
+            expires: cnLater
+        });
+
+        cnDomNode.fadeOut(300, function () {
+            self.removeCookieNotice();
+        });
     };
 
     $.fn.setSplashScreenNotice = function (cookie_value) {
 
         if (document.cookie.indexOf('cookie_notice_accepted') === -1) {
-
-            setTimeout(
-                function () {
-                    $(this).setSplashScreenNotice($(this).data('splashScreen-set'));
-                },
-                10000
-            );
             return;
         }
 
         var nextSplashShow = getCookie('next_splash_show');
-     
-        //var offset = 1 * 60 * 1000; //use for for debug
-        var offset = 24 * 60* 60 * 1000; //prod 24h
+
+        var offset = 1 * 60 * 1000; //use for for debug  1 minute
+       //var offset = 24 * 60 * 60 * 1000; //prod 24h
 
         if (!nextSplashShow || new Date() > new Date(nextSplashShow)) {
             var dtNextSplashShow = new Date();
@@ -96,22 +89,22 @@
             var expireTime = new Date();
             expireTime.setTime(parseInt(expireTime.getTime()) + 64281600 * 1000);
             var expireTimeSt = expireTime.toGMTString();
-            document.cookie = 'next_splash_show=' + encodeURIComponent(dtNextSplashShow) + ';expires=' + expireTimeSt +';';
+            document.cookie = 'next_splash_show=' + encodeURIComponent(dtNextSplashShow) + ';expires=' + expireTimeSt + ';';
             $('#splashscreen-overlay').show();
             setTimeout(
                 function () {
-                    alert('!');
+                   
                     $('#splashscreen-overlay').hide();
                 },
-                100000
+                10000
             );
         }
     };
 
-	// remove Cookie Notice
-	$.fn.removeCookieNotice = function ( cookie_value ) {
-		$( '#cookie-notice' ).remove();
-		$( 'body' ).removeClass( 'cookies-not-accepted' );
+    // remove Cookie Notice
+    $.fn.removeCookieNotice = function (cookie_value) {
+        $('#cookie-notice').remove();
+        $('body').removeClass('cookies-not-accepted');
     }
 
-} )( jQuery );
+})(jQuery);
