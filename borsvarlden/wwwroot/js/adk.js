62c7ac52923497acc2a5d86a439a73017e3368e0
Konstantin Arapov <konstantin@roxosoft.com>
2021-05-09 02:16:38 +0500

    ADK banner trying

---
+++ borsvarlden/wwwroot/js/adk.js
@@ -0,0 +1,21 @@
+﻿(function () {
+
+    function loadScript(src) {
+        var scriptElem = document.createElement('SCRIPT');
+        scriptElem.setAttribute('src', src);
+        document.head.appendChild(scriptElem)
+    }
+    function loadStyles(href) {
+        var linkElem = document.createElement('LINK');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', href);
+        document.head.appendChild(linkElem)
+    }
+
+    window.adk = window.adk || { cmd: [] }
+    var publicRoot = 'https://source.advisible.com/10124263'
+
+    loadScript('https://cdn.advisible.com/adk-1.5.8.js')
+    loadStyles(publicRoot + '/native/style.css')
+    loadScript(publicRoot + '/native/setup.js')
+})();
--- borsvarlden/wwwroot/js/site.js
+++ borsvarlden/wwwroot/js/site.js
@@ -2,70 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-(function ($) {
-    $(document).ready(function () {
-        // Recalc sticky right elements when subscribe widget terms is shown / hiden
-        $('#single-collapse-').on('shown.bs.collapse', function () {
-            $('.sticky-right').trigger("sticky_kit:recalc");
-        });
+(function($) {
+        $(document).ready(function() {
+            // Recalc sticky right elements when subscribe widget terms is shown / hiden
+            $('#single-collapse-').on('shown.bs.collapse',
+                function() {
+                    $('.sticky-right').trigger("sticky_kit:recalc");
+                });
 
-        $('#single-collapse-').on('hidden.bs.collapse', function () {
-            $('.sticky-right').trigger("sticky_kit:recalc");
-        });
+            $('#single-collapse-').on('hidden.bs.collapse',
+                function() {
+                    $('.sticky-right').trigger("sticky_kit:recalc");
+                });
 
-        // Handle form events / validations
-        $('#sidebar-widget-form- #signup-submit-1').click(function (e) {
-            e.preventDefault();
-            $("#sidebar-widget-form- #signup-submit").click();
-            return false;
-        });
-
-        $('#sidebar-widget-form-').validator().on('submit',
-            function(e) {
-                if (e.isDefaultPrevented()) {
-                    // Show terms if checkbox is not checked
-                    if (!$('#checkbox-terms-').is(":checked")) {
-                        $('#single-collapse-').collapse('show');
-                    }
-                } else {
-                    e.preventDefault(); // don't submit multiple times
-                    this.submit(); // use the native submit method of the form element
-
-                    setTimeout(function() { // Delay for Chrome
-                            $('#sidebar-widget-form- #mce-EMAIL').val(''); // blank the input
-                        },
-                        100);
-                }
+            // Handle form events / validations
+            $('#sidebar-widget-form- #signup-submit-1').click(function(e) {
+                e.preventDefault();
+                $("#sidebar-widget-form- #signup-submit").click();
+                return false;
             });
-        //Facebook injection proto  TODO remove
-      /*  $('iframe[name="f16aa1e321034e4"]').each(function () {
-            function injectCSS() {
-                $iframe.contents().find('head').append(
-                    $('<link/>', { rel: 'stylesheet', href: 'iframe.css', type: 'text/css' })
-                );
-            }
 
-            var $iframe = $(this);
-            $iframe.on('load', injectCSS);
-            injectCSS();
-        });
+            $('#sidebar-widget-form-').validator().on('submit',
+                function(e) {
+                    if (e.isDefaultPrevented()) {
+                        // Show terms if checkbox is not checked
+                        if (!$('#checkbox-terms-').is(":checked")) {
+                            $('#single-collapse-').collapse('show');
+                        }
+                    } else {
+                        e.preventDefault(); // don't submit multiple times
+                        this.submit(); // use the native submit method of the form element
 
-        $('table.article-table').stacktable();
-    });// End of document ready*/
-})(jQuery);
-
-//Facebook injection proto  TODO remove
-/*
-(function ($) {
-    $('iframe[name = "f16aa1e321034e4"]').ready(function() {
-        function injectCSS() {
-            $iframe.contents().find('head').append(
-                $('<link/>', { rel: 'stylesheet', href: '/css/site.min.css', type: 'text/css' })
-            );
-        }
-        alert('foo !')
-        var $iframe = $(this);
-        $iframe.on('load', injectCSS);
-        injectCSS();
-    });
-})(jQuery);*/
+                        setTimeout(function() { // Delay for Chrome
+                                $('#sidebar-widget-form- #mce-EMAIL').val(''); // blank the input
+                            },
+                            100);
+                    }
+                });
+        })(jQuery);
+    }
+);
