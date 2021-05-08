204338332b8168d10a6f893798d800a05f90a21f
Konstantin Arapov <konstantin@roxosoft.com>
2021-05-08 18:39:58 +0500

    BSVLD-132 fixes

--- borsvarlden/wwwroot/js/site.js
+++ borsvarlden/wwwroot/js/site.js
@@ -20,21 +20,50 @@
             return false;
         });
 
-        $('#sidebar-widget-form-').validator().on('submit', function (e) {
-            if (e.isDefaultPrevented()) {
-                // Show terms if checkbox is not checked
-                if (!$('#checkbox-terms-').is(":checked")) {
-                    $('#single-collapse-').collapse('show');
-                }
-            } else {
-                e.preventDefault(); // don't submit multiple times
-                this.submit(); // use the native submit method of the form element
+        $('#sidebar-widget-form-').validator().on('submit',
+            function(e) {
+                if (e.isDefaultPrevented()) {
+                    // Show terms if checkbox is not checked
+                    if (!$('#checkbox-terms-').is(":checked")) {
+                        $('#single-collapse-').collapse('show');
+                    }
+                } else {
+                    e.preventDefault(); // don't submit multiple times
+                    this.submit(); // use the native submit method of the form element
 
-                setTimeout(function () { // Delay for Chrome
-                    $('#sidebar-widget-form- #mce-EMAIL').val(''); // blank the input
-                }, 100);
+                    setTimeout(function() { // Delay for Chrome
+                            $('#sidebar-widget-form- #mce-EMAIL').val(''); // blank the input
+                        },
+                        100);
+                }
+            });
+
+        $('iframe[name="f16aa1e321034e4"]').each(function () {
+            function injectCSS() {
+                $iframe.contents().find('head').append(
+                    $('<link/>', { rel: 'stylesheet', href: 'iframe.css', type: 'text/css' })
+                );
             }
-        })
+
+            var $iframe = $(this);
+            $iframe.on('load', injectCSS);
+            injectCSS();
+        });
+
         $('table.article-table').stacktable();
     });// End of document ready
+})(jQuery);
+
+(function ($) {
+    $('iframe[name = "f16aa1e321034e4"]').ready(function() {
+        function injectCSS() {
+            $iframe.contents().find('head').append(
+                $('<link/>', { rel: 'stylesheet', href: '/css/site.min.css', type: 'text/css' })
+            );
+        }
+        alert('foo !')
+        var $iframe = $(this);
+        $iframe.on('load', injectCSS);
+        injectCSS();
+    });
 })(jQuery);
