3800a532490d8703ad4725cbd96680e217e81fb7
Konstantin Arapov <konstantin@roxosoft.com>
2021-06-27 01:59:02 +0500

    BSVLD-137 Redesign of A-Z details page. Also changed BennerRight  using

---
+++ borsvarlden/Views/Shared/Components/IndexGreenTagRight/IndexGreentagRight.cs
@@ -0,0 +1,32 @@
+﻿using borsvarlden.Helpers;
+using borsvarlden.Services.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace borsvarlden.Views.Shared.Components.IndexGreenTagFeed
+{
+    using ViewModels;
+    public class IndexGreentagRight : ViewComponent
+    {
+        private readonly IFinwireNewsService _finwireNewsService;
+        private readonly IConfigurationHelper _configurationHelper;
+
+        public IndexGreentagRight(IFinwireNewsService finwireNewsService, IConfigurationHelper configurationHelper)
+        {
+            _finwireNewsService = finwireNewsService;
+            _configurationHelper = configurationHelper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _finwireNewsService.GetAdvertiseNewsList(_configurationHelper.IndexNewsCount);
+            model.AddRange(await _finwireNewsService.GetMostReadNews(_configurationHelper.MostReadNewsCount, -1));
+            model.AddRange(await _finwireNewsService.GetNews(_configurationHelper.LatestNewsCount));
+
+            return View("IndexGreenTagRight", model.GroupBy(x => x.Id)
+                                                    .Select(x => x.First())
+                                                    .ToList());
+        }
+    }
+}
